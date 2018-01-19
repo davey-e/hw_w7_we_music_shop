a933251b574ef547471d1c95eba7ca9087716235
Dave Ellis <dave@ellisnetmail.co.uk>
2018-01-19 11:47:24 +0000

    Guitar class now implements IPlayable with play method and refactored type property to be called guitarType

--- src/main/java/Guitar.java
+++ src/main/java/Guitar.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+public class Guitar implements IPlayable{
 
-public class Guitar {
+    private int numberOfStrings;
+    private String guitarType;
+
+    public Guitar(int numberOfStrings, String type){
+        this.numberOfStrings = numberOfStrings;
+        this.guitarType = type;
+    }
+
+    public int getNumberOfStrings() {
+        return numberOfStrings;
+    }
+
+    public String getGuitarType() {
+        return guitarType;
+    }
+
+    public String play() {
+        return "Strumming on a guitar";
+    }
 }
--- src/test/java/GuitarTest.java
+++ src/test/java/GuitarTest.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class GuitarTest {
+
+    Guitar guitar;
+
+    @Before
+    public void before(){
+        guitar = new Guitar(5, "Bass");
+    }
+
+    @Test
+    public void hasNumberOfStrings(){
+        assertEquals(5, guitar.getNumberOfStrings());
+    }
+
+    @Test
+    public void hasGuitarType(){
+        assertEquals("Bass", guitar.getGuitarType());
+    }
+
+    @Test
+    public void canPlay(){
+        assertEquals("Strumming on a guitar", guitar.play());
+    }
+
 }
